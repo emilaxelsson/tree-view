ef15c085fd756982c2e9b9db873a5ae15e5a7fbe
emax <emax@chalmers.se>
2016-10-05 13:35:41 +0000

    Close handle after writing

--- src/Data/Tree/View.hs
+++ src/Data/Tree/View.hs
@@ -96,6 +96,7 @@ writeHtmlTree file tree = do
     h <- openFile file WriteMode
     hSetEncoding h utf8
     hPutStr h $ htmlTree $ tree
+    hClose h
 
 template1 =
   "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n\
